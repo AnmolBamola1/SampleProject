1f65dea655a8e17bd0f9938fcb2214bb282377cf
Anmol Bamola <anmol.bamola@gmail.com>
2022-07-10 15:27:43 +0530

    assignment

---
+++ script.js
@@ -0,0 +1,5 @@
+window.onresize = function(){
+    document.getElementById('element').innerHTML=`<p class="apptext"><img src="./images/mine.jpg" class="imgap"></img><strong>Hello</strong>this page is made by Anmol Bamola </p>`
+     
+   }
+
